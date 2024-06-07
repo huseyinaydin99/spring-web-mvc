34b5bc409da03de808680952204b61039208bdcd
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-06-07 13:04:51 +0300

    Spring Web MVC Parameterizable View Controller(parametrelendirilebilir görünüm/sayfa denetleyicisi).

---
+++ 017_ParameterizableViewController/src/main/java/tr/com/huseyinaydin/HomeController.java
@@ -0,0 +1,39 @@
+package tr.com.huseyinaydin;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+//بسم الله الرحمن الرحيم
+/**
+*
+* @author Huseyin_Aydin
+* @since 1994
+* @category Spring Web MVC
+*
+*/
+
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Hoşgeldin bacı gardaş nassın? {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		model.addAttribute("serverTime", formattedDate );
+		return "home";
+	}
+}
---
+++ 017_ParameterizableViewController/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,14 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	Hay Gayssss.! :D 
+</h1>
+
+<P>  Zunucunun zamanu bacı gardaşşşş! ${serverTime}. </P>
+</body>
+</html>
